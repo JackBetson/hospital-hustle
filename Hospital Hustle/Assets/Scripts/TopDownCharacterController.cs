4b323741d6bce6e2d4f479549ce2eea0e966554a
Jack Betson <jack.bet@outlook.com>
2024-04-15 00:12:41 +1000

    Player animation

--- Hospital Hustle/Assets/Scripts/TopDownCharacterController.cs
+++ Hospital Hustle/Assets/Scripts/TopDownCharacterController.cs
@@ -4,11 +4,11 @@ public class TopDownCharacterController : MonoBehaviour
 {
     public float speed;
 
-    //private Animator animator;
+    private Animator animator;
 
     private void Start()
     {
-        //animator = GetComponent<Animator>();
+        animator = GetComponent<Animator>();
     }
 
 
@@ -18,27 +18,27 @@ public class TopDownCharacterController : MonoBehaviour
         if (Input.GetKey(KeyCode.A))
         {
             dir.x = -1;
-            //animator.SetInteger("Direction", 3);
+            animator.SetInteger("Direction", 3);
         }
         else if (Input.GetKey(KeyCode.D))
         {
             dir.x = 1;
-            //animator.SetInteger("Direction", 2);
+            animator.SetInteger("Direction", 2);
         }
 
         if (Input.GetKey(KeyCode.W))
         {
             dir.y = 1;
-            //animator.SetInteger("Direction", 1);
+            animator.SetInteger("Direction", 1);
         }
         else if (Input.GetKey(KeyCode.S))
         {
             dir.y = -1;
-            //animator.SetInteger("Direction", 0);
+            animator.SetInteger("Direction", 0);
         }
 
         dir.Normalize();
-        //animator.SetBool("IsMoving", dir.magnitude > 0);
+        if (dir.magnitude == 0) animator.SetInteger("Direction", 4);
 
         GetComponent<Rigidbody2D>().velocity = speed * dir;
     }
