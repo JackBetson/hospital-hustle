4218d30c21f0a462fe28d4c78e0ec5003d698dbe
Zoe Wong <zoe3wong@gmail.com>
2024-04-21 22:11:51 +1000

    audio manager

--- Hospital Hustle/Assets/Scripts/Dialogue/IntroDialogue.cs
+++ Hospital Hustle/Assets/Scripts/Dialogue/IntroDialogue.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class StartDialogue : MonoBehaviour
+public class IntroDialogue : MonoBehaviour
 {
     public string[] lines;
     public float dialogueSpeed = 0.05f;
