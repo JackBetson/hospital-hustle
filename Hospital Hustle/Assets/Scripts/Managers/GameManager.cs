efe06a52792b1fe21103f98f8db1fac58eb38335
Jack Betson <jack.bet@outlook.com>
2024-04-21 17:27:02 +1000

    Random room selection

--- Hospital Hustle/Assets/Scripts/Managers/GameManager.cs
+++ Hospital Hustle/Assets/Scripts/Managers/GameManager.cs
@@ -1,8 +1,15 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
+
+    [SerializeField] private TextMeshProUGUI _notificationText;
+    private GameObject[] _patientRooms;
+    private string _currentTargetRoomId;
+    private const string MAIN_LEVEL_NAME = "MainLevel";
 
     void Awake()
     {
@@ -10,6 +17,7 @@ public class GameManager : MonoBehaviour
         {
             Instance = this;
             DontDestroyOnLoad(gameObject);
+            SceneManager.sceneLoaded += OnSceneLoaded;
         }
         else
         {
@@ -17,5 +25,73 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    // Add any other management functions below
+    private void Start()
+    {
+        StartNewRound();
+    }
+
+    void OnDestroy()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == MAIN_LEVEL_NAME)
+        {
+            Debug.Log("Main level loaded");
+            RefreshRoomReferences();
+            if (_currentTargetRoomId != null)
+            {
+                UpdateNotification(_currentTargetRoomId);
+            }
+        }
+    }
+
+    private void RefreshRoomReferences()
+    {
+        _patientRooms = GameObject.FindGameObjectsWithTag("PatientRoom");
+        if (_patientRooms.Length == 0)
+        {
+            Debug.LogError("No patient rooms found!");
+        }
+    }
+
+    public void StartNewRound()
+    {
+        GameObject selectedRoom = SelectRandomRoom();
+        if (selectedRoom != null)
+        {
+            _currentTargetRoomId = selectedRoom.name;
+            UpdateNotification(_currentTargetRoomId);
+        }
+    }
+
+    private GameObject SelectRandomRoom()
+    {
+        if (_patientRooms == null || _patientRooms.Length == 0)
+        {
+            Debug.LogError("No patient rooms available!");
+            return null;
+        }
+        int randomIndex = Random.Range(0, _patientRooms.Length);
+        return _patientRooms[randomIndex];
+    }
+
+    private void UpdateNotification(string roomId)
+    {
+        if (_notificationText != null)
+        {
+            _notificationText.text = "Please go to Room: " + roomId;
+        }
+        else
+        {
+            Debug.LogError("Notification text UI is not set!");
+        }
+    }
+
+    public string GetCurrentTargetRoomId()
+    {
+        return _currentTargetRoomId;
+    }
 }
